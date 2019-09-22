f4abc031ac789f0060bbebe27a121da96863b917
Ball1nUkra1n3 <jason.s.m@hotmail.com>
2019-09-22 14:51:18 -0400

    Add files via upload
    CHANGELOG:
    --made camera follow player
    --added secondary texture on player
    --change test level a bit so you can see how physics work
    --cleaned up player script, moved key press and gravity code to their own respective functions
    
    TO DO:
    --get player sprite to rotate when moving
    --make level restart when touching rainbow texture at the bottom
    --change background
    --add music

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -11,17 +11,19 @@ const SPEED = 250
 const GRAVITY = 15
 const JUMP_HEIGHT = -600
 
+#variable to determine if player released jump button already to stop jumping
+#turns to true so you can't do it more than once mid-air and cheat the game
+#resets to false when you touch the ground again
+var releasedJumpAlready = false
+
 #variable to store motion of ball sprite, does X and Y together
 #access with motion.x and motion.y
 var motion = Vector2()
 
-#function for movement of ball sprite that is updated every frame (delta)
-func _physics_process(delta):
-	
-	#sets "gravity" of player as constant downward motion
-	#only happens when not colliding with the floor
-	if !(is_on_floor()):
-		motion.y += GRAVITY
+func get_input():
+	#close game if escape key pressed
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 	
 	#sets motion based on actions defined in Project>Project Settings>Input Map
 	#this allows you to easily map it to other things like gamepads or touchscreens
@@ -33,17 +35,37 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 		
+	#if the player releases the jump button, it sets y motion to 0 and player falls
+	#this is done so player can make short jumps, but not cheat and float or double jump or any shit
+	if Input.is_action_just_released("ball_jump"):
+		if motion.y < 0 && releasedJumpAlready == false:
+			motion.y = 0
+			releasedJumpAlready = true
+			
+	#inherited from KinBody2D that actually processes the motion we're setting with the keys above
+	move_and_slide(motion, UP)
+			
+func calc_gravity():
+	#sets "gravity" of player as constant downward motion
+	#only happens when not colliding with the floor
+	if !(is_on_floor()):
+		motion.y += GRAVITY
+		
 	#if the player is actually on the floor and presses the jump button it will make them jump
 	if is_on_floor():
+		releasedJumpAlready = false
 		if Input.is_action_just_pressed("ball_jump"):
 			motion.y = JUMP_HEIGHT
 			
-	#function to close game if escape key pressed
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+	#if player hits a ceiling, make player fall back down
+	#note you can't set this to 0 or it will stick to the ceiling
+	if is_on_ceiling():
+		motion.y = 1
 
-	#function inherited from KinBody2D that actually processes the motion we're setting with the keys above
-	move_and_slide(motion, UP)
+#function for movement of ball sprite that is updated every frame (delta)
+func _physics_process(delta):
+	get_input()
+	calc_gravity()
 	pass
 
 
