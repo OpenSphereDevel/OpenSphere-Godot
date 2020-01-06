fd9363c0d0ac047081001b0a8ab7466dbca35ae2
Ball1nUkra1n3 <jason.s.m@hotmail.com>
2020-01-06 18:40:52 -0500

    Add files via upload
    --Added main menu now with functioning Play and Quit buttons.  Play only brings you to the test world I made because I don't have any real levels made yet
    --Music now plays on test level. It's the cloud theme on a loop.
    --Touching rainbow floor now kills you, restarting level
    --R can restart the level
    
    TODO:
    --Make music not restart when level is restarted
    --Add death counter that stays persistent with level changes
    --Implement color detection for color specific platforms
    --Make death floor only kill you if it's touching PLAYER, not any collision body in general

---
+++ levels/mainmenu/NewGameButton.gd
@@ -0,0 +1,7 @@
+extends Button
+
+#somehow this magically knows when button is pressed
+#created using the node/signals thing
+#for now only loads the level template, obviously no real levels made yet
+func _on_NewGameButton_pressed():
+	get_tree().change_scene("res://levels/template/WorldTemplate.tscn")
